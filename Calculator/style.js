b9d1745da36a9110979ada4fe317e4a59b606fc2
Eduardo Miury <edmiury@gmail.com>
2023-05-30 16:24:21 -0300

    third-app

---
+++ Calculator/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const Style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    txtInput: {
+        marginBottom: 20,
+        borderWidth: 0.5,
+        borderRadius: 10,
+        padding: 10,
+        fontWeight: 'bold'
+    }
+})
